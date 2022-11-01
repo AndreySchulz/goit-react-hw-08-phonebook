6620df0a9335d920f19620b44ead30440ceba40a
Andrey_Schulz <andreygorb040693@gmail.com>
2022-11-01 12:04:45 +0200

    complite

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,15 @@ import ContactList from './ContactList/ContactList';
 
 export class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: JSON.parse(localStorage.getItem('contacts')) ?? [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   inputValueForm = e => {
     const { value } = e.target;
 
