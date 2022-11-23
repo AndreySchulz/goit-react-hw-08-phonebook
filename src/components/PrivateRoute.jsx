495d80769ad83551dc8fdbac0031ffd3bd0a30f4
Andrey_Schulz <andreygorb040693@gmail.com>
2022-11-23 19:45:56 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+
 import { logOutUser } from 'redux/auth/authOperation';
 import { getUserEmail } from 'redux/auth/authSelectors';
 import Button from '@mui/material/Button';
